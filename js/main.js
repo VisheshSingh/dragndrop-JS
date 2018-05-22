a219451ba5ecc97f86936721cc19409527ef8de6
Vishesh <visheshsingh16@gmail.com>
2018-05-22 15:53:43 -0500

    drag drop events

--- js/main.js
+++ js/main.js
@@ -5,6 +5,14 @@ const empties = document.querySelectorAll(".empty");
 fill.addEventListener("dragstart", dragStart);
 fill.addEventListener("dragend", dragEnd);
 
+//loop thru empties and call drag events
+for (const empty of empties) {
+  empty.addEventListener("dragover", DragOver);
+  empty.addEventListener("dragenter", DragEnter);
+  empty.addEventListener("dragleave", DragLeave);
+  empty.addEventListener("drop", DragDrop);
+}
+
 //Drag functions
 function dragStart() {
   this.className += " hold";
@@ -13,4 +21,22 @@ function dragStart() {
 
 function dragEnd() {
   this.className = "fill";
+}
+
+function DragOver(e) {
+  e.preventDefault();
+}
+
+function DragEnter(e) {
+  e.preventDefault();
+  this.className += " hovered";
+}
+
+function DragLeave() {
+  this.className = "empty";
+}
+
+function DragDrop() {
+  this.className = "empty";
+  this.append(fill);
 }
